d4e53f599566667576a6c4051b63350ad6fc7457
look a look <1024726590@qq.com>
2026-07-20 17:10:37 +0800

    Update server.py for deployment

--- server.py
+++ server.py
@@ -411,5 +411,6 @@ def api_export_review():
     )
 
 if __name__ == "__main__":
-    app.run(debug=True, host="127.0.0.1", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
 
